bd8e62a8b318440b8f3e036d4d2f5f8b2889c963
Tania <yolisma2149@gmail.com>
2024-06-07 20:20:44 -0600

    Agregar archivos de proyecto.

---
+++ SumaAppMvvm/SumaPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using SumaAppMvvm.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaAppMvvm.Views
+{
+    public partial class SumaPage : ContentPage
+    {
+        public SumaViewModel ViewModel { get; set; }
+
+        public SumaPage()
+        {
+            InitializeComponent();
+            ViewModel = new SumaViewModel();
+            BindingContext = ViewModel;
+        }
+
+        private void SumarClicked(object sender, System.EventArgs e)
+        {
+            ViewModel.SumarCommand.Execute(null);
+        }
+
+        private void LimpiarClicked(object sender, System.EventArgs e)
+        {
+            ViewModel.LimpiarCommand.Execute(null);
+        }
+    }
+}
---
+++ SumaAppMvvm/SumaViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SumaAppMvvm.ViewModels
+{
+    public class SumaViewModel : INotifyPropertyChanged
+    {
+        private string _resultado;
+
+        public string Resultado
+        {
+            get => _resultado;
+            set
+            {
+                _resultado = value;
+                OnPropertyChanged(nameof(Resultado));
+            }
+        }
+
+        public ICommand SumarCommand => new Command(() =>
+        {
+            if (string.IsNullOrEmpty(Valor1) || string.IsNullOrEmpty(Valor2))
+            {
+                Resultado = "Por favor ingrese ambos valores.";
+            }
+            else
+            {
+                Resultado = (int.Parse(Valor1) + int.Parse(Valor2)).ToString();
+            }
+        });
+
+        public ICommand LimpiarCommand => new Command(() =>
+        {
+            Valor1 = string.Empty;
+            Valor2 = string.Empty;
+            Resultado = string.Empty;
+        });
+
+        private string _valor1;
+        public string Valor1
+        {
+            get => _valor1;
+            set
+            {
+                _valor1 = value;
+                OnPropertyChanged(nameof(Valor1));
+            }
+        }
+
+        private string _valor2;
+        public string Valor2
+        {
+            get => _valor2;
+            set
+            {
+                _valor2 = value;
+                OnPropertyChanged(nameof(Valor2));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
